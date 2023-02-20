dd6745df93b71e6cb4bbdfc7bafb0eb3e46cd46e
AnaVucic <anavucic007@gmail.com>
2023-02-20 22:30:58 +0100

    added id for response

--- Airline company - backend/airlinecompany/app/Http/Resources/UserResource.php
+++ Airline company - backend/airlinecompany/app/Http/Resources/UserResource.php
@@ -16,6 +16,7 @@ class UserResource extends JsonResource
     public function toArray($request)
     {
         return [
+            'id' => $this->resource->id,
             'username' => $this->resource->username,
             'name' => $this->resource->name,
             'role'=>$this->resource->role
